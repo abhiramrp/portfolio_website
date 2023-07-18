070ec75f5449b09f731702c239ccddc2d918558e
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2023-07-18 11:38:15 -0500

    changed profile pic

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,13 +2,20 @@ import Image from "next/image";
 
 export default function Intro() {
     return (
-        <div>
+        <div className="flex">
 
-            <h1>Hey, Im Abhiram 👋</h1>
-            <p>
-                Im a software developer based in Lagos, Nigeria. I specialize in building (and occasionally designing)
-                exceptional websites, applications, and everything in between.
-            </p>
+            <div>
+                <Image src="/intro.jpg" className="ml-auto" alt="Abhiram's face"  width={500} height={500} />
+            </div>
+
+            <div>
+                <h1>Hey, Im Abhiram 👋</h1>
+            </div>
+
+            <div>
+                Im a software developer based in Dallas-Forth metroplex, TX. I am currently looking for entry level roles in software development, cloud computinng, and machine learning. 
+            </div>
+
         </div>
     )
 }
