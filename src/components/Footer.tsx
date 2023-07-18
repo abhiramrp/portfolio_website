67cb50376042188e198e2908f842b4e373d42603
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2023-07-18 06:12:30 -0500

    done with footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,49 @@
 import Link from "next/link";
+import Image from "next/image";
+
+import { EnvelopeIcon } from "@heroicons/react/24/solid";
 
 export default function Footer() {
     return (
-        <div className="footer-container">
-            <p>
-                Footer
-            </p>
-            <div className="social_icons">
-                asdfsdf
-                <Link
-                    href="https://github.com/abhiramrp"
-                    aria-label="GitHub"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i className="fa-brands fa-github"></i>
-                </Link>
-                <Link
-                    href="https://www.linkedin.com/in/abhiramprattipati/"
-                    aria-label="LinkedIn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i className="fa-brands fa-linkedin"></i>
-                </Link>
+        
+        <footer className="bg-gray-600 text-white mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="md:flex md:items-center md:justify-between py-2">
+                <span className="text-sm">
+                    © {new Date().getFullYear()} Abhiram Rishi Prattipati
+                </span>
+
+                <div className="ml-4 flex justify-center items-center space-x-4">
+
+                    <div className="w-10 h-10 relative">
+                        <Link href="mailto:pabhiramrishi@gmail.com">
+                            <EnvelopeIcon className="h-10" />
+                        </Link>
+                    </div>
+
+                    <div className="w-10 h-10 relative p-2">
+                        <Link href="https://github.com/abhiramrp">
+                            <Image sizes="100%" layout="fill" objectFit="cover" quality={100} src="/logos/github.svg" alt="github"  />
+                        </Link>
+                    </div>
+
+                    <div className="w-12 h-10 relative">
+                        <Link href="https://www.linkedin.com/in/abhiramprattipati/">
+
+                            <Image sizes="100%" layout="fill" objectFit="cover" quality={100} src="/logos/linkedin.svg" alt="github"  />
+                        </Link>
+                    </div>
+
+
+                </div>
             </div>
-        </div>
+
+
+        </footer>
     )
 }
+
+
+/**
+ * <Image fill={true} src="/logos/github.svg" alt="github"  />
+ */
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
                     Resume
                 </Link>
                 <Link href="/contact" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    About
+                    Contact
                 </Link>
                 
                 </div>
