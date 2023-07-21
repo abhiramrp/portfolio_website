0d81a6fc03b80bac05d2cb672a50b9dd4b8978a5
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2023-07-20 22:57:50 -0500

    need to work on navbar menu from scratch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,11 @@ import { useState } from 'react';
 
 import ThemeSwitcher from "@/app/themeswitcher";
 
-import {Bars3Icon} from "@heroicons/react/24/solid";
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
 
 export default function Navbar() {
 
-    const [navActive, setNavActive] = useState(false);
+    const [navbar, setNavbar] = useState(false);
 
     return (
 
@@ -24,25 +24,57 @@ export default function Navbar() {
             </div>
             <div className="flex sm:block justify-between items-center ">
                     
-                <div className="ml-4 flex space-x-4">
+              
+
                     <ThemeSwitcher />
-                <Link href="#intro" className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Home
-                </Link>
-                <Link href="#about" className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    About
-                </Link>
-                <Link href="/projects" className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Projects
-                </Link>
-                <Link href="https://drive.google.com/file/d/1UtM5LFFq9-iRM1oE2ZtzYwRQ4LF87mKZ/view?usp=sharing" className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Resume
-                </Link>
-                <Link href="/contact" className="text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                    Contact
-                </Link>
-                
-                </div>
+
+                    <div className="md:hidden">
+
+                        <Bars3Icon className={`w-10 h-10 ${navbar ? (  <XMarkIcon className="w-10 h-10" /> ) : ( <Bars3Icon className="w-10 h-10"/> )}`} role="button" onClick={() => setNavbar(!navbar)} />
+
+                    </div>
+
+                    <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                navbar ? 'p-12 md:p-0 block' : 'hidden'
+              }`}>
+
+                        <ul className="h-screen md:h-auto items-center justify-center md:flex ">
+
+                        <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  border-purple-900  md:hover:text-white md:hover:bg-transparent">
+                            <Link href="#intro" onClick={() => setNavbar(!navbar)}>
+                                Home
+                            </Link>
+                        </li>
+
+                        <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  border-purple-900  md:hover:text-white md:hover:bg-transparent">
+                            <Link href="#about" onClick={() => setNavbar(!navbar)}>
+                                About
+                            </Link>
+                        </li>
+
+                        <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  border-purple-900  md:hover:text-white md:hover:bg-transparent">
+                            <Link href="/projects" onClick={() => setNavbar(!navbar)}>
+                                Projects
+                                </Link>
+                        </li>
+
+                        <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  border-purple-900  md:hover:text-white md:hover:bg-transparent">
+                            <Link href="https://drive.google.com/file/d/1UtM5LFFq9-iRM1oE2ZtzYwRQ4LF87mKZ/view?usp=sharing" onClick={() => setNavbar(!navbar)}>
+                                Resume
+                                </Link>
+                        </li>
+
+                        <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  border-purple-900  md:hover:text-white md:hover:bg-transparent">
+                            <Link href="/contact" onClick={() => setNavbar(!navbar)}>
+                                Contact
+                            </Link>
+
+                        </li>
+
+                        </ul>
+
+                    </div>
+             
             </div>
             </div>
         </nav>
@@ -50,3 +82,13 @@ export default function Navbar() {
 
  
 }
+
+
+
+
+/*
+<Bars3Icon role="button" onClick={() => setNavbar(!navbar)}> 
+                            {navbar ? (  <XMarkIcon /> ) : ( <Bars3Icon /> )}
+                        
+                        </Bars3Icon>
+*/
